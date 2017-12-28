66906c4eb8ef4247e8324f5a77a85f9adc87fd20
Igor V. Kruglenko <igor.kruglenko@gmail.com>
2017-12-28 16:05:56 +0200

    Homework #7

---
+++ script.js
@@ -0,0 +1,34 @@
+echo= (str)=>console.log(str);
+
+/*
+Визуализируем клавиатуру.
+
+Написать метод createLayout() который создает клавиатуру состоящую из 3-х строк, в каждой строке такое кол-во кнопок как длины topRow/middleRow и bottomRow полей.
+Можно использовать либо createElement либо создавать строку из тегов и вешать через insertAdjacentHTML или innerHTML.
+Добавить минимальное оформление на свой вкус.
+*/
+
+function Keyboard(){
+	let keys = {
+		topRow: '',
+		middleRow: '',
+		bottomRow: ''
+	},
+		out="";
+	this.createLayout = (alphabet)=>{
+		alphabet.slice (0,12).split('').forEach((char, index)=>keys.topRow += createKey(char));
+		alphabet.slice (12,24).split('').forEach((char, index)=>keys.middleRow += createKey(char));
+		alphabet.slice (24).split('').forEach((char, index)=>keys.bottomRow += createKey(char));
+		out +=`<div class="topRow">${keys.topRow}</div>`
+		out +=`<div class="middleRow">${keys.middleRow}</div>`
+		out +=`<div class="bottomRow">${keys.bottomRow}</div>`
+		out = `<div class="keyboard">${out}</div>`
+	}
+
+	let createKey = (key) =>`<div class="key">${key}</div>`;
+	this.render=(id)=>document.getElementById(id).innerHTML=out;
+}
+
+let keyboard = new Keyboard();
+keyboard.createLayout("qwertyuiop[]asdfghjkl;'\\zxcvbnm,./")
+keyboard.render("root");
